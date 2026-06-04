3484d36d609a38bbedeb36beb20c47025daa3a30
TheN1cer <andy97916@gmail.com>
2026-06-04 09:46:53 +0000

    程式本體

--- Final project.py
+++ Final project.py
@@ -13,13 +13,13 @@ except FileNotFoundError:
 
 #清理標點符號及換行符號, punctuation包含了所有的標點符號。
 import string
-text = text.translate(str.maketrans("", "", string.punctuation)).replace("\n", " ")
+text = text.translate(str.maketrans("", "", string.punctuation))
 
 # 2. 文字切割 (將字串轉為 List)
-word_list = text.split(" ")
+word_list = text.split()
 
 # 3. 過濾雜訊 (設定 Tuple 黑名單)
-stop_words = ("is", "and", "the", "a", "of", "to", "in", "that", "it", "with", "as", "for", "was", "on", "are", "by", "this", "be", "or", "from", "at", "which", "but", "not", "all", "we", "they", "their", "his", "her", "my", "your", "its")
+stop_words = ("is", "and", "the", "a", "of", "to", "in", "that", "it", "with", "as", "for", "was", "on", "are", "by", "this", "be", "or", "from", "at", "which", "but", "not", "all", "we", "they", "their", "his", "her", "my", "your", "its", "me", "him", "you", "i", "has", "have", "do", "does", "did", "he", "she", "them", "what", "who", "when", "where", "why", "how")
 filtered_words = []
 for word in word_list:
     # 簡單的清理：將單字轉小寫，方便比對
@@ -38,10 +38,11 @@ for word in filtered_words:
 # 5. 排序並輸出結果
 result = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
 
-# 6. 設定門檻並過濾資料
-threshold = 3  # 只顯示出現次數大於或等於 2 的關鍵字
-filtered_result = [item for item in result if item[1] >= threshold]
-print (f"出現次數大於或等於 {threshold} 的關鍵字:")
+# 6. 改為取前 20 筆資料
+limit = 30
+filtered_result = result[:limit]  # 利用切片 [start:stop]
+
+print(f"出現次數最多的前 {limit} 個關鍵字:")
 for word, count in filtered_result:
     print(f"{word:<12}: {count}次")
 
@@ -53,7 +54,7 @@ if len(filtered_result) > 0:
     # --- 繪製長條圖 ---
     plt.figure(figsize=(10, 6))
     plt.bar(words, values, color='skyblue')
-    plt.title(f"Top Keywords (Frequency >= {threshold})")
+    plt.title(f"Top {limit} Keywords")
     plt.xlabel("Words")
     plt.ylabel("Frequency")
     plt.xticks(rotation=45)
@@ -63,13 +64,13 @@ if len(filtered_result) > 0:
 
     # --- 繪製文字雲 ---
     # 使用 generate_from_frequencies 直接接收統計字典
-    wc = WordCloud(width=800, height=400, background_color='white', colormap='viridis')
+    wc = WordCloud(width=1920, height=1080, background_color='white', colormap='inferno')
     wc.generate_from_frequencies(word_counts)
     
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(16, 9))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis('off')
-    plt.savefig("wordcloud.png")
+    plt.savefig("wordcloud.png", dpi=120, bbox_inches='tight')
     print("文字雲已儲存為 wordcloud.png")
     
 else:
